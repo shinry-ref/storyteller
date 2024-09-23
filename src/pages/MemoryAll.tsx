0a6cd7584a846859018aa6f40693c8eaf9f8957f
shinry <refdoragon>
2024-09-24 01:06:05 +0900

    修正

--- src/pages/MemoryAll.tsx
+++ src/pages/MemoryAll.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardBody, Flex, Heading, Link, Stack, Tag, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, Flex, Heading, Link, Spinner, Stack, Tag, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DeleteStory, getStories, getUser } from "../utils/supabaseFunction";
@@ -11,16 +11,22 @@ export const MemoryAll = () => {
   const navigate = useNavigate();
   const [stories, setStories] = useState<story[]>([]);
   const [user, setUser] = useState<User>();
+  const [loading, setLoading] = useState(true); // ローディング状態を追加
 
   useEffect(() => {
     const getUserRecord = async () => {
       try {
         const newStories = await getStories();
+        const sortedStories = newStories.sort((a, b) => {
+          return new Date(b.story_date).getTime() - new Date(a.story_date).getTime();
+        });
         const newUser = await getUser(1);
-        setStories(newStories);
+        setStories(sortedStories);
         setUser(newUser);
       } catch (error) {
         console.error("Failed to fetch records:", error);
+      } finally {
+        setLoading(false); // データ取得後にローディングを終了
       }
     };
     getUserRecord();
@@ -34,7 +40,10 @@ export const MemoryAll = () => {
     try {
       await DeleteStory(id);
       const newStories = await getStories();
-      setStories(newStories);
+      const sortedStories = newStories.sort((a, b) => {
+        return new Date(b.story_date).getTime() - new Date(a.story_date).getTime();
+      });
+      setStories(sortedStories);
     } catch (error) {
       console.error('Error deleting story:', error);
     }
@@ -55,27 +64,34 @@ export const MemoryAll = () => {
               <Text ml={2} fontWeight="bold">{user?.name}</Text>
             </Flex>
             <Heading textAlign="center" mb={4} as='h4' size='md' color="black">すべての思い出</Heading>
-            <Box maxH='400px' overflowY='auto'> {/* スクロール可能なボックスを追加 */}
-              <Stack spacing={4} mt={4}>
-                {stories.map((story) => (
-                  <Box key={story.id} p={4} borderWidth={1} borderRadius="md" bg="white">
-                    <Heading size="sm" mb={2}>{story.title}（{story?.story_date ? new Date(story.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
-                    <Flex align="center" mb={2}>
-                      <Tag colorScheme="blue" variant="solid">{story.category_name}</Tag>
-                    </Flex>
-                    <Flex justify='flex-end' mt={2}>
-                      <Stack direction='row' spacing={4}>
-                        <Button onClick={() => onClickDetail(story.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
-                          詳細
-                        </Button>
-                        <Button colorScheme='red' onClick={() => onClickDelete(story.id)} leftIcon={<MdDelete />}>
-                          削除
-                        </Button>
-                      </Stack>
-                    </Flex>
-                  </Box>
-                ))}
-              </Stack>
+            <Box maxH='400px' overflowY='auto'>
+              {/* ローディング中はスピナーを表示 */}
+              {loading ? (
+                <Flex justify='center' align='center'>
+                  <Spinner size="xl" color="teal.500" />
+                </Flex>
+              ) : (
+                <Stack spacing={4} mt={4}>
+                  {stories.map((story) => (
+                    <Box key={story.id} p={4} borderWidth={1} borderRadius="md" bg="white">
+                      <Heading size="sm" mb={2}>{story.title}（{story?.story_date ? new Date(story.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
+                      <Flex align="center" mb={2}>
+                        <Tag colorScheme="blue" variant="solid">{story.category_name}</Tag>
+                      </Flex>
+                      <Flex justify='flex-end' mt={2}>
+                        <Stack direction='row' spacing={4}>
+                          <Button onClick={() => onClickDetail(story.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
+                            詳細
+                          </Button>
+                          <Button colorScheme='red' onClick={() => onClickDelete(story.id)} leftIcon={<MdDelete />}>
+                            削除
+                          </Button>
+                        </Stack>
+                      </Flex>
+                    </Box>
+                  ))}
+                </Stack>
+              )}
             </Box>
           </CardBody>
         </Card>
--- src/pages/MemoryHome.tsx
+++ src/pages/MemoryHome.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardBody, Flex, Heading, Link, Stack, Tag, Text } from "@chakra-ui/react";
+import { Box, Button, Card, CardBody, Flex, Heading, Link, Spinner, Stack, Tag, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DeleteStory, getStories, getUser } from "../utils/supabaseFunction";
@@ -13,44 +13,60 @@ export const MemoryHome = () => {
   const [latestStories, setLatestStories] = useState<story[]>([]);
   const [user, setUser] = useState<User>();
   const [randomStory, setRandomStory] = useState<story | null>(null);
+  const [isLoading, setIsLoading] = useState(true); // ローディング状態を管理
 
   useEffect(() => {
-    const getUserRecord = async () =>{
+    const getUserRecord = async () => {
       try {
         const newStories = await getStories();
+
+        const sortedStories = newStories.sort((a, b) => {
+          return new Date(b.story_date).getTime() - new Date(a.story_date).getTime();
+        });
         const newUser = await getUser(1);
-        const newlatestStories = newStories.slice(0, 2);
+        const newlatestStories = sortedStories.slice(0, 2);
         setLatestStories(newlatestStories); // 最新の3件を取得
-        setStories(newStories);
-        setRandomStory(getRandomStory(newStories.filter(story => !newlatestStories.includes(story))));
+        setStories(sortedStories);
+        setRandomStory(getRandomStory(sortedStories.filter(story => !newlatestStories.includes(story))));
         setUser(newUser);
-      } catch (error){
+      } catch (error) {
         console.error("Failed to fetch records:", error);
+      } finally {
+        setIsLoading(false); // データ取得後にローディング終了
       }
-    }
+    };
     getUserRecord();
   }, []);
 
   const onClickDetail = (id: number) => {
     navigate(`/memories/${id}`);
-  }
+  };
 
   const onClickDelete = async (id: number) => {
     try {
+      setIsLoading(true); // 削除時もローディングを開始
       await DeleteStory(id);
       const newStories = await getStories();
-      setStories(newStories);
+      const sortedStories = newStories.sort((a, b) => {
+        return new Date(b.story_date).getTime() - new Date(a.story_date).getTime();
+      });
+
+      const newlatestStories = sortedStories.slice(0, 2);
+      setLatestStories(newlatestStories);
+      setStories(sortedStories);
+      setRandomStory(getRandomStory(sortedStories.filter(story => !newlatestStories.includes(story))));
     } catch (error) {
       console.error('Error deleting story:', error);
+    } finally {
+      setIsLoading(false); // 削除完了後にローディング終了
     }
-  }
+  };
 
   const getRandomStory = (stories: story[]) => {
     if (stories.length === 0) return null;
     const randomIndex = Math.floor(Math.random() * stories.length);
     return stories[randomIndex];
   };
-
 
   return (
     <>
@@ -60,56 +76,66 @@ export const MemoryHome = () => {
             <Flex justify='space-around' borderRadius='md' mb={4}>
               <Link href='/memories/register' color="black" fontWeight="bold" mr={1} onClick={() => navigate('/memories/register')} textDecoration="underline" _hover={{ color: 'teal.500' }}>新規登録</Link>
               <Link href='/memories/calendar' color="black" fontWeight="bold" mr={1} textDecoration="underline" _hover={{ color: 'teal.500' }}>カレンダー</Link>
-            </Flex>  
+            </Flex>
             <Flex align="center" mb={4}>
               <FaUserCircle size={24} />
               <Text ml={2} fontWeight="bold">{user?.name}</Text>
             </Flex>
-            <Heading textAlign="center" mb={4} as='h4' size='md' color="black">思い出を物語にしよう</Heading>
-            <Stack spacing={4} mt={4}>
-              {randomStory && (
-                <Box p={4} borderWidth={1} borderRadius="md" bg="pink.100" mb={4}>
-                  あの日の思い出
-                  <Heading size="sm" mb={2}>{randomStory.title}（{randomStory?.story_date ? new Date(randomStory.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
-                  <Flex align="center" mb={2}>
-                    <Tag colorScheme="blue" variant="solid">{randomStory.category_name}</Tag>
-                  </Flex>
-                  <Flex justify='flex-end' mt={2}>
-                    <Stack direction='row' spacing={4}>
-                      <Button onClick={() => onClickDetail(randomStory.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
-                        詳細
-                      </Button>
-                      <Button colorScheme='red' onClick={() => onClickDelete(randomStory.id)} leftIcon={<MdDelete />}>
-                        削除
-                      </Button>
-                    </Stack>
-                  </Flex>
-                </Box>
-              )}
 
-              {latestStories.map((story) => (
-                <Box key={story.id} p={4} borderWidth={1} borderRadius="md" bg="white">
-                  <Heading size="sm" mb={2}>{story.title}（{story?.story_date ? new Date(story.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
-                  <Flex align="center" mb={2}>
-                    <Tag colorScheme="blue" variant="solid">{story.category_name}</Tag>
-                  </Flex>
-                  <Flex justify='flex-end' mt={2}>
-                    <Stack direction='row' spacing={4}>
-                      <Button onClick={() => onClickDetail(story.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
-                        詳細
-                      </Button>
-                      <Button colorScheme='red' onClick={() => onClickDelete(story.id)} leftIcon={<MdDelete />}>
-                        削除
-                      </Button>
-                    </Stack>
-                  </Flex>
-                </Box>
-              ))}
-            </Stack>
-            {stories.length >= 3 && (
-              <Link href="/memories/all" color="blue.500" mt={4} textAlign="center" display="block">
-                もっと見る
-              </Link>
+            {/* ローディング中かどうかの確認 */}
+            {isLoading ? (
+              <Flex justify='center' align='center' h='300px'>
+                <Spinner size="xl" /> {/* Chakra UI のスピナー */}
+              </Flex>
+            ) : (
+              <>
+                <Heading textAlign="center" mb={4} as='h4' size='md' color="black">思い出を物語にしよう</Heading>
+                <Stack spacing={4} mt={4}>
+                  {randomStory && (
+                    <Box p={4} borderWidth={1} borderRadius="md" bg="pink.100" mb={4}>
+                      あの日の思い出
+                      <Heading size="sm" mb={2}>{randomStory.title}（{randomStory?.story_date ? new Date(randomStory.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
+                      <Flex align="center" mb={2}>
+                        <Tag colorScheme="blue" variant="solid">{randomStory.category_name}</Tag>
+                      </Flex>
+                      <Flex justify='flex-end' mt={2}>
+                        <Stack direction='row' spacing={4}>
+                          <Button onClick={() => onClickDetail(randomStory.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
+                            詳細
+                          </Button>
+                          <Button colorScheme='red' onClick={() => onClickDelete(randomStory.id)} leftIcon={<MdDelete />}>
+                            削除
+                          </Button>
+                        </Stack>
+                      </Flex>
+                    </Box>
+                  )}
+
+                  {latestStories.map((story) => (
+                    <Box key={story.id} p={4} borderWidth={1} borderRadius="md" bg="white">
+                      <Heading size="sm" mb={2}>{story.title}（{story?.story_date ? new Date(story.story_date).toLocaleDateString() : '日付未設定'}）</Heading>
+                      <Flex align="center" mb={2}>
+                        <Tag colorScheme="blue" variant="solid">{story.category_name}</Tag>
+                      </Flex>
+                      <Flex justify='flex-end' mt={2}>
+                        <Stack direction='row' spacing={4}>
+                          <Button onClick={() => onClickDetail(story.id)} colorScheme='teal' leftIcon={<MdVisibility />}>
+                            詳細
+                          </Button>
+                          <Button colorScheme='red' onClick={() => onClickDelete(story.id)} leftIcon={<MdDelete />}>
+                            削除
+                          </Button>
+                        </Stack>
+                      </Flex>
+                    </Box>
+                  ))}
+                </Stack>
+                {stories.length >= 3 && (
+                  <Link href="/memories/all" color="blue.500" mt={4} textAlign="center" display="block">
+                    もっと見る
+                  </Link>
+                )}
+              </>
             )}
           </CardBody>
         </Card>
